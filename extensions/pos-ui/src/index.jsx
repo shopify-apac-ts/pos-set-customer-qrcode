af216acb06e4707efc75ae5d6d6aaa30cbc628df
Nobu Hayashi <nobu.hayashi@shopify.com>
2024-03-28 14:38:33 +0900

    working sample

---
+++ extensions/pos-ui/src/index.jsx
@@ -0,0 +1,67 @@
+import {
+  React,
+  useEffect, 
+  }  from 'react'
+import { 
+  CameraScanner,
+  Tile, 
+  Text, 
+  Screen, 
+  Stack,
+  Navigator, 
+  render,
+  useCartSubscription,
+  useExtensionApi,
+  useScannerDataSubscription 
+} from '@shopify/retail-ui-extensions-react'
+
+
+  const SmartGridTile = () => {
+    const api = useExtensionApi()
+    return (
+      <Tile
+        title="Read Customer ID"
+        subtitle="Open QR / barcode reader"
+        onPress={() => {
+          api.smartGrid.presentModal()
+        }}
+        enabled
+      />
+    )
+  }
+
+// SmartGridModal component that adds a scanned product to the cart
+const SmartGridModal = () => {
+  // Use hooks to get the extension API and scanner data
+  const api = useExtensionApi();
+  const { data } = useScannerDataSubscription();
+  const cart = useCartSubscription();
+
+  // Use an effect to add the scanned product to the cart when data changes
+  useEffect(() => {
+    // TODO: add logic to identify & associate customer
+    const addCustomerToCart = (data) => {
+      if (data) {
+        api.cart.setCustomer({id: Number(data)});
+      }
+    };
+    addCustomerToCart(data);
+  }, [data]);
+
+  const scannedData = `${cart?.customer?.email || ''}`
+  return (
+    <Navigator>
+      <Screen name="reader_screen" title={scannedData} >
+        <Stack direction="vertical" flexChildren flex={1}>
+          <CameraScanner />
+        </Stack>
+      </Screen>
+    </Navigator>
+  );
+};
+
+// <Text>{`Scanned data: ${data || ''}`}</Text>
+
+
+  render('pos.home.tile.render', () => <SmartGridTile />)
+  render('pos.home.modal.render', () => <SmartGridModal />)
